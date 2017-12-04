c18dfa67309ef247dd6d32c6c0100add872afb85
Roman Kharitonov <rkharito@yandex.ru>
2017-12-05 00:32:27 +1000

    Remove obsolete scripts

--- remux.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set FN=%~dpn1
-set SUFFIX=%2
-set SRC_FLAGS=--no-audio --no-subtitles --no-track-tags --no-attachments --no-buttons --no-chapters --no-global-tags
-set ENC_FLAGS=--no-video
-mkvmerge -o "%FN%_mux.mkv" %SRC_FLAGS% "%FN%_%2.mkv" %ENC_FLAGS% "%FN%.mkv"
